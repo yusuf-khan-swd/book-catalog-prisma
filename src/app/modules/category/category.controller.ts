ecd514a018a5d10cb11d0d14961f84618c6fe369
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-09-01 00:08:19 +0600

    Create a controller for create category

--- src/app/modules/category/category.controller.ts
+++ src/app/modules/category/category.controller.ts
@@ -1,1 +1,19 @@
-export const CategoryController = {};
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { CategoryService } from './category.service';
+
+const createCategory = catchAsync(async (req: Request, res: Response) => {
+  const data = req.body;
+  const result = await CategoryService.createCategory(data);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Category created successfully',
+    data: result,
+  });
+});
+
+export const CategoryController = { createCategory };
