153753a1a1df8a490ff28ac936b544d188cf2fe7
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-08-31 23:16:39 +0600

    Create a route for delete user

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -8,5 +8,6 @@ const router = express.Router();
 router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
 router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleUser);
 router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.updateUser);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deleteUser);
 
 export const UserRoutes = router;
