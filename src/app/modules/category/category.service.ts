3f381295e097b80dd6676d14e82b8a8d6ac58547
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-09-01 00:08:10 +0600

    Create a service for create category

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,1 +1,8 @@
-export const CategoryService = {};
+import { Category } from '@prisma/client';
+import prisma from '../../../shared/prisma';
+
+const createCategory = async (data: Category): Promise<Category> => {
+  return await prisma.category.create({ data });
+};
+
+export const CategoryService = { createCategory };
