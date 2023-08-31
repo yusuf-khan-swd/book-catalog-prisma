1fc5a6f311f6885672d707691881ca510b1bc18c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-08-31 23:00:36 +0600

    secure route to only admin

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middleware/auth';
 import { UserController } from './user.controller';
 
 const router = express.Router();
 
-router.get('/', UserController.getAllUsers);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
 
 export const UserRoutes = router;
