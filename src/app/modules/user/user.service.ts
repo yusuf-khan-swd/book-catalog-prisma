834a78e1e0e812a19298e3f29afd1357e2509069
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-08-31 22:25:00 +0600

    Create a service for create user

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,1 +1,10 @@
-export const UserService = {};
+import { User } from '@prisma/client';
+import prisma from '../../../shared/prisma';
+
+const createUser = async (data: User): Promise<User> => {
+  return await prisma.user.create({
+    data,
+  });
+};
+
+export const UserService = { createUser };
