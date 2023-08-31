658dd09c3b8ec285a109cb9aa5fa452a54ec1c47
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-08-31 23:05:48 +0600

    Create a route for get single user

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -6,5 +6,6 @@ import { UserController } from './user.controller';
 const router = express.Router();
 
 router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleUser);
 
 export const UserRoutes = router;
