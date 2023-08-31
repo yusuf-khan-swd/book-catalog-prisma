f670ef26156982230ce08a423a43464d75ee9f84
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-08-31 22:30:48 +0600

    Create a route for get all user

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import { UserController } from './user.controller';
 
 const router = express.Router();
+
+router.get('/', UserController.getAllUsers);
 
 export const UserRoutes = router;
