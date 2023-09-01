7b704f4366882905fcce73833a9b01aab90b3ac2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-09-01 14:58:33 +0600

    Create routes for   createBook,   getAllBooks,   getSingleBook,   updateBook,   deleteBook,

---
+++ src/app/modules/book/book.routes.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middleware/auth';
+import { BookController } from './book.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-book',
+  auth(ENUM_USER_ROLE.ADMIN),
+  BookController.createBook
+);
+
+router.get('/', BookController.getAllBooks);
+router.get('/:id', BookController.getSingleBook);
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), BookController.updateBook);
+router.delete('/:id', BookController.deleteBook);
+
+export const BookRoutes = router;
