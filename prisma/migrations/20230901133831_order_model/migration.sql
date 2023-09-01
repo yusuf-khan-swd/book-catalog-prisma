86c9a8029162cafd6a278dc7451890fc51c16d5c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-09-01 19:38:49 +0600

    migrate order model

---
+++ prisma/migrations/20230901133831_order_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('pending', 'shipped', 'delivered');
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "orderedBooks" JSONB[],
+    "status" "Status" NOT NULL DEFAULT 'pending',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
