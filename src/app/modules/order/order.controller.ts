c4e7bcda601dfda2cc90da70d1d9cba753247018
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-09-01 22:32:27 +0600

    Create controllers for   createOrder,   getAllOrders,   getSingleOrder,

---
+++ src/app/modules/order/order.controller.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { OrderService } from './order.service';
+
+const createOrder = catchAsync(async (req: Request, res: Response) => {
+  const data = req.body;
+  const userId = req.user?.userId;
+  data.userId = userId;
+  const result = await OrderService.createOrder(data);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Category created successfully',
+    data: result,
+  });
+});
+
+const getAllOrders = catchAsync(async (req: Request, res: Response) => {
+  const result = await OrderService.getAllOrders();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Get all categories successfully',
+    data: result,
+  });
+});
+
+const getSingleOrder = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await OrderService.getSingleOrder(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Get single category successfully',
+    data: result,
+  });
+});
+
+export const OrderController = {
+  createOrder,
+  getAllOrders,
+  getSingleOrder,
+};
